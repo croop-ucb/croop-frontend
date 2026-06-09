08f3aa914d021b3e91438d8ae567341a4a378cb6
GabriellCabrall <gabrielbielcabral@gmail.com>
2026-06-09 16:18:31 -0300

    Corrige logo do croop e lista de espécies

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -1,14 +1,13 @@
-import api from './api'; 
-import { NotificacaoResponse } from '../types/api'; 
+import api from './api';
+import { NotificacaoResponse } from '../types/api';
 
 export const notificationService = {
   getNotificacoes: async (): Promise<NotificacaoResponse[]> => {
-    
-    const response = await api.get<NotificacaoResponse[]>('/notificacoes');
+    const response = await api.get<NotificacaoResponse[]>('/notificacoes/');
     return response.data;
   },
 
   marcarComoLida: async (idNotificacao: number): Promise<void> => {
-    await api.patch(`/notificacoes/${idNotificacao}/ler`);
-  }
+    await api.patch(`/notificacoes/${idNotificacao}/lida`);
+  },
 };
